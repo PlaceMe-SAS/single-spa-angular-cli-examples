d235107721d532eabf450f2c9b6c395c94a4ed20
Robin Coma Delperier <robin@placeme.io>
2018-01-09 22:47:54 +0100

    fix(micro app): not load for cli serve

--- single-spa-angular-mf/src/single-spa-angular-platform.ts
+++ single-spa-angular-mf/src/single-spa-angular-platform.ts
@@ -2,29 +2,38 @@ import { Router } from "../../src/home/node_modules/@angular/router";
 
 declare const window;
 
-const isSingleSpaApp = () => {
-    return document.getElementById;
-};
-
 class SingleSpaAngularPlatform {
 
     appName: string;
 
     mount(appName: string, callback: Function): void {
-        this.appName = appName;
-        window[this.appName] = {};
-        window[this.appName].mount = () => {
-            callback(this.unmount.bind(this));
-        };
+        if (this.isSingleSpaApp()) {
+            this.appName = appName;
+            window[this.appName] = {};
+            window[this.appName].mount = () => {
+                callback(this.unmount.bind(this));
+            };
+        } else {
+            setTimeout(() => {
+                callback();
+            }, 1);
+        }
+
     }
 
     unmount(module: any) {
-        window[this.appName].unmount = () => {
-            if (module) {
-                module.destroy();
-                module.injector.get(Router).dispose();
-            }
-        };
+        if (this.isSingleSpaApp()) {
+            window[this.appName].unmount = () => {
+                if (module) {
+                    module.destroy();
+                    module.injector.get(Router).dispose();
+                }
+            };
+        }
+    }
+
+    private isSingleSpaApp (): boolean {
+        return document.querySelector('body').hasAttribute('data-single-spa');
     }
 }
 
