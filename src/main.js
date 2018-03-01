40ec00e91ed3ac61810cb9deba2158f4b1f979d8
Robin Coma Delperier <robin@placeme.io>
2018-03-01 14:51:45 +0100

    feat(doc & version): merge with joeldenning and update doc and deps

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import { singleSpaAngularCliRouter } from 'single-spa-angular-cli/lib/utils';
 import 'babel-polyfill';
 import 'zone.js';
 
-registerApplication('menu', () => import('./loaders/menu.js'), singleSpaAngularCliRouter.hashPrefix('/**', true));
-registerApplication('home', () => import('./loaders/home.js'), singleSpaAngularCliRouter.hashPrefix('/home', true));
-registerApplication('app1', () => import('./loaders/app1.js'), singleSpaAngularCliRouter.hashPrefix('/app1'));
-registerApplication('help', () => import('./loaders/help.js'), singleSpaAngularCliRouter.hashPrefix('/app1'));
+registerApplication('menu', import('./loaders/menu.js'), singleSpaAngularCliRouter.hashPrefix('/**'));
+registerApplication('home', import('./loaders/home.js'), singleSpaAngularCliRouter.hashPrefix('/home', true));
+registerApplication('app1', import('./loaders/app1.js'), singleSpaAngularCliRouter.hashPrefix('/app1'));
+registerApplication('help', import('./loaders/help.js'), singleSpaAngularCliRouter.hashPrefix('/app1'));
 start();
